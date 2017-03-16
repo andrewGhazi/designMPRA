bf76c3445cca783343624dd912733e9fc2291781
Andrew Ghazi <andghazi@gmail.com>
2017-03-16 15:30:10 -0500

    Mostly filled out power and sequence gen pages

--- server.R
+++ server.R
@@ -6,18 +6,85 @@
 #
 
 library(shiny)
+library(tidyverse)
+library(pwr)
+library(VariantAnnotation)
+library(Biostrings)
+library(BSgenome.Hsapiens.UCSC.hg38)
+source('scripts/processVCF.R')
+
 
 shinyServer(function(input, output) {
 
-  output$distPlot <- renderPlot({
+  output$powerPlot <- renderPlot({
 
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    plotDat = data_frame(meanDiff = seq(0,5, by = .05),
+                         pwr = pwr.t.test(n = input$nbarcode,
+                                          d = meanDiff / input$sigma,
+                                          sig.level = .05 / input$nsnp)$power)
+    
+    ggplot(plotDat, aes(meanDiff, pwr)) + 
+      geom_line() + 
+      xlab('Transcriptional Shift (absolute value)') + 
+      ylab('Power to detect difference in 1 transfection') + 
+      ylim(0,1)
 
   })
+  
+  inVCF = reactive({
+    inVCF = input$vcf
+    
+    if (is.null(inVCF)){
+      return(NULL)
+    }
+    
+    skipNum = system(paste0('grep ^## ', inVCF$datapath, ' | wc -l'), intern = TRUE) %>% as.numeric
+    
+    #Check that the header doesn't have spaces in place of tabs. If it does, replace the spaces with tabs and create a new col_names variable
+    vcfColumns = system(paste0('head -', skipNum + 1, ' ~/plateletMPRA/data/CD36_initial75_dbSNP.vcf | tail -1'), 
+                        intern = TRUE) %>% 
+      gsub('#', '', .) %>% 
+      gsub('[ ]+', '\t', .) %>% #replace spaces with tabs if applicable
+      str_split('\t') %>% 
+      unlist
+    
+    read_tsv(inVCF$datapath, 
+             skip = skipNum + 1,
+             col_names = vcfColumns)
+  })
+  
+  vcfOut = eventReactive(input$Go, {
+    inVCF() %>% processVCF(input$nBCperSNP, input$contextWidth)
+  })
+
+  
+  output$inputHead = renderTable(
+    head(inVCF())
+  )
+  
+  output$outputHead = eventReactive(input$Go,{
+    validate(
+      need(input$nBCperSNP > 0,
+           'Please input a number of barcodes per snp to proceed')
+    )
+    
+    validate(
+      need(nrow(inVCF()) > 0,
+           'Please input a VCF')
+    )
+    
+    validate(
+      need(nrow(vcfOut) > 0,
+           'Sequence generation failed')
+    )
+    
+    head(vcfOut())
+  })
+  
+  output$downloadSequences = downloadHandler(
+    'out.tsv',
+    content = function(file){
+      write.csv(vcfOut(), file)
+    })
 
 })
--- ui.R
+++ ui.R
@@ -10,21 +10,87 @@ library(shiny)
 shinyUI(fluidPage(
 
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("MPRA design tools"),
 
-  # Sidebar with a slider input for number of bins
   sidebarLayout(
-    sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-    ),
-
-    # Show a plot of the generated distribution
+    sidebarPanel(conditionalPanel(condition = 'input.selectedTab==3',
+                                  numericInput('nBCperSNP',
+                                               label = h6('n barcodes per snp'),
+                                               value = 0),
+                                  numericInput('contextWidth',
+                                               label = h6('Genomic context width (1/2 construct width)'),
+                                               value = 75),
+                                  fileInput('vcf',
+                                            label = 'Input VCF'),
+                                  actionButton('Go',
+                                               label = 'Go!'),
+                                  downloadButton('downloadSequences', 'Download Output'))),
     mainPanel(
-      plotOutput("distPlot")
+      tabsetPanel(
+        tabPanel("MPRA", 
+                 p("Massively parallel reporter assays (MPRA) are high-throughput assays designed to inspect the transcriptional effects of many variants in parallel. See the diagram below for a graphical explanation."),
+                 br(),
+                 p("The \"activity\" of an allele is defined as the log-ratio of the output mRNA count to the input DNA count for a given allele. Because there is significant noise in these counts, allele constructs are replicated across multiple barcodes. The observed activity of each barcode is taken as one observation of the activity of the allele. The \"transcriptional shift\" of a variant is defined as the difference in activity between the mutant and reference alleles. Thus variants that increase transcription (e.g. the red variant in the diagram below) have a positive transcriptional shift and those that decrease transcription (e.g. the blue variant) have a negative shift."),
+                 br(),
+                 img(src = 'formulas.png',
+                     align = 'center',
+                     width = 728,
+                     height = 102),
+                 p("This application aims to help researchers understand the relationships between power, noise, and experimental design in MPRAs. Optimizing experimental design is critical for sensitivity in a MPRA assay -- the variance of how many transcripts a single cell makes from a single DNA molecule can be extremely high. Multiple testing corrections further reduce sensitivity. Repeated transfections combined with quantile normalization to remove batch effects can be an effective way of improving power, however this comes at the price of increased sequencing needs."),
+                 img(src = 'MPRAdiagram.png',
+                     align = 'center',
+                     width = 723,
+                     height = 947),
+                 p(''),
+                 br(),
+                 p(''),
+                 value = 1),
+        tabPanel("Power", 
+                 p('Here you can inspect the interaction between the various parameters of a MPRA assay and their effect on the power the detect transcriptional shifts of a given size. Note that activity (and thus transcriptional shift) are log values, and thus a transcriptional shift of +4 means the mutant allele produces exp(+5) = 148.4 times as much mRNA per input DNA molecule.'),
+                 numericInput('sigma',
+                              label = h4('Activity standard deviation'),
+                              value = 1.6),
+                 numericInput('nsnp',
+                              label = h4('Number of snps tested in assay'),
+                              value = 8000),
+                 selectInput('parameter',
+                             label = h4('Choose parameter to investigate'),
+                             choices = list('n' = 1, 
+                                            'Power' = 2,
+                                            'Difference in mean activity' = 3,
+                                            'significance level' = 4),
+                             selected = 3),
+                 numericInput('nbarcode',
+                              label = h4('n barcodes per snp'),
+                              value = 20),
+                 p('This plot shows the power of a t-test to distinguish differential activity in a variant at a Bonferonni corrected alpha = .05 level. While activity measurements do not strictly satisfy the assumptions of a t-test (they are not normally distributed), in practice a normal approximation works well.'),
+                 br(),
+                 plotOutput('powerPlot'),
+                 value = 2),
+        tabPanel("Create sequences",
+                 p('Here you can upload a VCF to automatically generate the necessary construct sequences for your own MPRA experiment. Only the CHROM, POS, REF, and ALT columns are used. Current constraints are: '),
+                 tags$div(
+                   tags$ul(
+                     tags$li('sequence context comes from the hg38 assembly'),
+                     tags$li('Restriction sites are limited to XbaI, KpnI, and SfiI as they are used in MPRA protocol published in Melnikov (2014).'),
+                     tags$li('Some snps may fail due to their sequence context containing or generating an undesired restriction site'),
+                     tags$li('Barcodes are constrained to being 10bp in length')
+                   )
+                 ),
+                 p('The outputs of this application are purely to make designing MPRA experiments more convenient for researchers. We make no guarantee as to the accuracy of the outputs and highly encourage you to methodically check your sequences before synthesizing your construct library.'),
+                 br(),
+                 h4('Input head:'),
+                 tableOutput('inputHead'),
+                 br(),
+                 h4('Output head:'),
+                 tableOutput('outputHead'),
+                 value = 3),
+        id = 'selectedTab'
+      )
     )
+    
   )
-))
+
+    
+  )
+)
