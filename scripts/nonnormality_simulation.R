2da7ec48eb9d6d915ffe652c47b3def7d208aff1
Andrew Ghazi <andghazi@gmail.com>
2017-09-11 01:45:09 -0500

    made marginally faster with map and fewer calls to nrow()
    added plot of difference

--- scripts/nonnormality_simulation.R
+++ scripts/nonnormality_simulation.R
@@ -15,39 +15,39 @@ worst %>%
 
 worst_mean = mean(worst$activity)
 
+one_simulation = function(x, transcription_shift, n_samp){
+  # randomly sample the most non-normal allele
+  non_normal_samples = sample(worst$activity, size = n_samp, replace = TRUE) 
+  
+  # randomly sample a "typical" allele
+  normal_samples = rnorm(n_samp, 
+                         mean = worst_mean - transcription_shift, 
+                         sd = .926) # using the average SD from Ulirsch and Tewhey together
+  
+  # compute the p-value of a t-test
+  t.test(non_normal_samples, normal_samples)$p.value
+}
+
+# used to calculate effect size
 pooled_SD = sqrt(((nrow(worst) - 1)*sd(worst$activity)**2 + (nrow(worst) - 1)*.926**2) / (nrow(worst)*2 - 2))
 
 simulate_power = function(transcription_shift, sig_level = .05, n_sim = 10000){
   
   #simulate a t-test many times
-  monte_carlo_p_values = sapply(1:n_sim, function(x){
-    
-    # randomly sample the most non-normal allele
-    non_normal_samples = sample(worst$activity, size = nrow(worst), replace = TRUE) 
-    
-    # randomly sample a "typical" allele
-    normal_samples = rnorm(nrow(worst), 
-                           mean = worst_mean - transcription_shift, 
-                           sd = .926) # using the average SD from Ulirsch and Tewhey together
-    
-    # compute the p-value of a t-test
-    t.test(non_normal_samples, normal_samples)$p.value
-  })
+  monte_carlo_p_values = map_dbl(1:n_sim, one_simulation, transcription_shift = transcription_shift, n_samp = nrow(worst))
   
-  # return how the fraction that pass the input significance level
+  # return the fraction that pass the input significance level
   sum(monte_carlo_p_values < sig_level) / n_sim
 }
 
-sim_power = data_frame(transcription_shift = seq(-1.5, 1.5, length.out = 200),
+sim_power = data_frame(transcription_shift = seq(-1.1, 1.1, length.out = 50),
                        theoretical_power = pwr.t.test(n = nrow(worst),
                                                       d = abs(transcription_shift) / pooled_SD,
                                                       type = 'two.sample',
-                                                      alternative = 'two.sided')$power,
-                       monte_carlo_power = mclapply(transcription_shift, simulate_power, mc.cores = 22) %>% unlist)
+                                                      alternative = 'two.sided',
+                                                      sig.level = 1e-5)$power,
+                       monte_carlo_power = mclapply(transcription_shift, simulate_power, mc.cores = 20, sig_level = 1e-5, n_sim = 3e6) %>% unlist)
 
-sim_power %>% 
-  gather(power_type, power, -transcription_shift) %>% 
-  ggplot(aes(transcription_shift, power, color = power_type)) + 
-  geom_path() +
-  scale_y_continuous(breaks = seq(0, 1, by = .1)) +
-  ggtitle('Monte Carlow simulation comparing non-normal allele\nt-test results and theoretical results')
+sim_power %>%
+  mutate(diff = monte_carlo_power - theoretical_power) %>%
+  ggplot(aes(transcription_shift, diff)) + geom_path()
